cbe23a9f2775355ab43b8bdf3b3a4cc1d1c9ce74
Orel-Viktor <OrelViktor93@gmail.com>
2022-09-30 15:51:13 +0300

    homework-redy

--- main.js
+++ main.js
@@ -6,21 +6,48 @@ let textSwapColor = document.querySelector('.js--textSwapColor')
 let buttonForSwapColor = document.querySelector('.js--buttonForSwapColor')
 textSwapColor.style.color = 'black'
 
-
-
 buttonForSwapColor.addEventListener('click', swapColor)
-
-
-// buttonForSwapColor.removeEventListener('click' , swapColor)
-
 
 function swapColor() {
   if (textSwapColor.style.color === 'black') {
     textSwapColor.style.color = 'red'
   }
-  else if (textSwapColor.style.color !=='black') {
+  else {
     textSwapColor.style.color = 'black'
   }
 }
+
+
+
+// Вывести таблицу 10×10, заполненную числами от 1 до 100 (таблица создана через js)
+
+
+let elem = document.querySelector('.js--elem')
+
+function createTable(parent, rows, cols) {
+  let table = document.createElement('table')
+
+  for (let i = 0; i < rows; i++) {
+    let tr = document.createElement('tr')
+
+    for (let j = 0; j < cols; j++) {
+      let td = document.createElement('td')
+      td.classList.add('tdWidth')
+      td.style.width = '40px'
+      td.style.height = '40px'
+      td.innerText = i*10 +j +1
+      tr.appendChild(td)
+
+    }
+    table.appendChild(tr)
+  }
+  parent.appendChild(table)
+}
+
+
+createTable(elem, 10, 10)
+let td = document.querySelectorAll(".js--elem td")
+console.log(td)
+
 
 
